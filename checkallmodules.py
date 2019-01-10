ca74da7e407ef8031399d4eba10350c35d95b116
dlb213 <dlb213@lehigh.edu>
2019-01-10 11:44:41 -0500

    updated the cas2 module lists, because I copied them weirdly.  Got the script working, didn't check on the version flag.  Need to add a command line option for printing details, so I am integrating optparse.  This commit is before adding that library into the code.

--- checkallmodules.py
+++ checkallmodules.py
@@ -2,58 +2,80 @@ import os,datetime, sys
 
 #This checks the modules on this server and indicates which are missing from the other 2 servers
 
-def parsemodulelist(modulefile):
-    line = modulefile.readline()
+def parsemodulelist(modulefile,v):
+    #set some variables up
+    if v:
+        lnlen = 3
+    else:
+        lnlen = 2
     modules = {}
-    
+    line = modulefile.readline()
+    n=1
     while line:
         if line.strip() == "":
             line = modulefile.readline()
+            n+=1
             continue
         else:
             linelist = line.split(',')
-            package = linelist[0]
-            #Access control,Block Access (block_access),Enabled,7.x-1.6
-            modulename = linelist[1].split('(')[0].strip()
-            machinename = linelist[1].split('(')[1].strip(')')
-            status = linelist[2]
-            version = linelist[3].strip()
-            line = modulefile.readline()
-            modules[machinename] = [modulename,version,status,package]
-            line = modulefile.readline()
+            if len(linelist)>lnlen and linelist[1].find('(')>0:
+                package = linelist[0]
+                #Access control,Block Access (block_access),Enabled,7.x-1.6
+                modulename = linelist[1].split('(')[0].strip()
+                machinename = linelist[1].split('(')[1].strip(')')
+                status = linelist[2]
 
+                if v:
+                    version = linelist[3].strip()
+                    modules[machinename] = [modulename,version,status,package]
+                else:
+                    modules[machinename] = [modulename,'--',status,package]
+                #next line
+                line = modulefile.readline()
+                n+=1
+            else:
+                print "ERROR READING line "+str(n)+" printing line: "
+                print line
+                line = modulefile.readline()
+                n+=1
+                
     return modules
             
 
 
 def main():
 
-    version = false
+    version = False
     if len(sys.argv) > 1:
         if sys.argv[1].startswith('--'):
             if sys.argv[1] == '--version':
-                version = true
+                version = True
                 
-    
+    print "reading in CAS2 modules"
     cas2file = open('cas2modules.csv','r')
-    cas2modules = parsemodulelist(cas2file)
+    cas2modules = parsemodulelist(cas2file,version)
     cas2file.close()
-    
+
+    print "reading in CAS3 modules"
     cas3file = open('cas3modules.csv','r')
-    cas3modules = parsemodulelist(cas3file)
+    cas3modules = parsemodulelist(cas3file,version)
     cas3file.close()
 
     #I am also going to add a file here for checking that all of the modules on the default database are also on the site
-    
+    here=os.getcwd()
     #now get the currently installed modules on this server
-    os.chdir('/var/www/drupal/sites/')
+
     #backup the old list, why not
     now = datetime.datetime.now()
-    os.system('mv currentmoduelist.csv modulelistbackups/currentmodulelist-'+now.strftime('%m%d%y%H%M')+'.csv')
+    os.system('mv currentmodulelist.csv modulelistbackups/currentmodulelist-'+now.strftime('%m%d%y%H%M')+'.csv')
+    
+    os.chdir('/var/www/drupal/sites/')
     os.system('drush pm-list --type=module --format=csv > /home/dlb213/usedscripts/check_installedmodules/currentmodulelist.csv')
 
+    os.chdir(here)
+    
     listfile = open('currentmodulelist.csv','r')
-    modulelist = parsemodulelist(listfile)
+    modulelist = parsemodulelist(listfile,version)
     listfile.close()
 
     #now I guess loop through each cas2 and cas3 list and see which are the same.
