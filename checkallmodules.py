fc0a140bfc7212253e2e3d33c89aaa10484002cd
dlb213 <dlb213@lehigh.edu>
2019-01-10 15:23:12 -0500

    Didn't integrate command args yet. Just cleaned up the display and the way it prints stuff out.

--- checkallmodules.py
+++ checkallmodules.py
@@ -21,8 +21,19 @@ def parsemodulelist(modulefile,v):
             if len(linelist)>lnlen and linelist[1].find('(')>0:
                 package = linelist[0]
                 #Access control,Block Access (block_access),Enabled,7.x-1.6
-                modulename = linelist[1].split('(')[0].strip()
-                machinename = linelist[1].split('(')[1].strip(')')
+                #Node export,Node export relation (deprecated) (node_export_relation),Not installed,7.x-3.1
+                names = linelist[1].split('(')
+                if len(names)>2:
+                    #this recreates the module and correctly generates the machine name if there is a parenthesis in the module display name
+                    machinename = names[-1].strip(')')
+                    modulename = names[0]
+                    for chars in names[1:-1]:
+                        modulename = modulename + "("+chars
+
+                else:
+                    modulename = names[0].strip()
+                    machinename = names[1].strip(')')
+
                 status = linelist[2]
 
                 if v:
@@ -68,22 +79,59 @@ def main():
     #backup the old list, why not
     now = datetime.datetime.now()
     os.system('mv currentmodulelist.csv modulelistbackups/currentmodulelist-'+now.strftime('%m%d%y%H%M')+'.csv')
-    
+    print "generating current module list"
     os.chdir('/var/www/drupal/sites/')
     os.system('drush pm-list --type=module --format=csv > /home/dlb213/usedscripts/check_installedmodules/currentmodulelist.csv')
 
     os.chdir(here)
-    
+    print "reading in current module list"
     listfile = open('currentmodulelist.csv','r')
     modulelist = parsemodulelist(listfile,version)
     listfile.close()
 
+    missunion = {}
+    missintersection = {}
     #now I guess loop through each cas2 and cas3 list and see which are the same.
-
+    print ""
+    print " -------------  PRINTING CAS 2  MISSING MODULES --------------------- "
+    print ""
+    misscas2 = {}
     for cas2 in sorted(cas2modules):
         if cas2 not in modulelist.keys():
-            print cas2modules[cas2][0]+" ( "+cas2+" )  is on cas2, NOT on this server"
+            misscas2[cas2] = cas2modules[cas2]
+            missunion[cas2] = cas2modules[cas2]
+            print cas2modules[cas2][0]+" -- "+cas2
+
+    print ""
+    print " -------------  PRINTING CAS 3  MISSING MODULES --------------------- "
+    print ""
+    misscas3 = {}
+    for cas3 in sorted(cas3modules):
+        if cas3 not in modulelist.keys():
+            misscas3[cas3] = cas3modules[cas3]
+            print cas3modules[cas3][0]+" -- "+cas3
+            if cas3 in missunion.keys():
+                missintersection[cas3] = cas3modules[cas3]
+            else:
+                missunion[cas3] = cas3modules[cas3]
+                
+    print ""
+    print " ------------- MODULES MISSING UNION FROM BOTH SERVERS--------------------- "
+    print ""            
     
+    for u in sorted(missunion):
+        print missunion[u][0]+" -- "+u
+
+        
+    print ""
+    print " ------------- MODULES MISSING INTERSECTION FROM BOTH SERVERS--------------------- "
+    print ""            
+    
+    for i in sorted(missintersection):
+        print missintersection[i][0]+" -- "+i
+
+    
+            
     # I could add a flag to the script that checks the version of the modules in the other lists and prints out the comparisons.
     # To Do this look for python command flag integration libraries
 
